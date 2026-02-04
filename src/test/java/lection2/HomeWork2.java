140f6f77218ca07ad0f1fcae2249f03c29674216
Milia <milia.menzatova@gmail.com>
2026-02-03 23:57:17 -0500

    home

---
+++ src/test/java/lection2/HomeWork2.java
@@ -0,0 +1,46 @@
+package lection2;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+        /*
+        Задача №1
+ Необходимо создать целочисленные переменные a и b, присвоить произвольные значения переменным на ваш
+ выбор и вывести результаты следующих операций с этими переменными: сложение, умножение, вычитание,
+ деление и остаток от деления. Также сделать проверку на четность этих переменных и вывести результат.
+         */
+
+        int a = 100;
+        int b = 50;
+
+        int c = 28;
+        int d = 11;
+
+        System.out.println("сложение " + (a + b));
+        System.out.println( "умножение " + (a * b));
+        System.out.println("вычитание " + (a - b));
+        System.out.println("деление " + (a / b));
+        System.out.println("остаток " + (a % b));
+
+        if(c % 2 == 0){
+            System.out.println("this is even number");
+        }
+
+        else{
+            System.out.println("this is odd number");
+        }
+        if(d % 2 == 0){
+            System.out.println("this is even number");
+        }
+
+        else{
+            System.out.println("this is odd number");
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ src/test/java/lection2/Lesson.java
@@ -0,0 +1,18 @@
+package lection2;
+
+public class Lesson {
+
+    public static void main(String[] args) {
+
+
+        int a = 10;
+        int b = 20;
+
+        boolean c = a == b;
+
+        if(a <= b){
+            System.out.println("a less then b");
+
+        }
+    }
+}
