feb1c55927578e7b605dcca5e2b2c90d5f4fb76c
Milia <milia.menzatova@gmail.com>
2026-02-10 22:35:44 -0500

    home

---
+++ src/test/java/lection4.java
@@ -0,0 +1,11 @@
+public class lection4 {
+    public static void main(String[] args) {
+        char c1 = 'A';
+        char c = 'A';
+        char b = 64;
+
+
+        System.out.println(c1 + 1);
+        System.out.println(b);
+    }
+}
