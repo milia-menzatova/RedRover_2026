ab765c57377bd9a8c86a7742f7711a1021dbaf2c
Milia <milia.menzatova@gmail.com>
2026-02-10 23:50:07 -0500

    home

--- src/test/java/lection/lection3/ArrayDemo.java
+++ src/test/java/lection/lection3/ArrayDemo.java
@@ -1,19 +1,24 @@
 package lection.lection3;
 
+import java.util.Arrays;
+
 public class ArrayDemo {
     public static void main(String[] args) {
         int a = 10;
         int[] array = {5, 4, 3};
-
         array[0] = 10;
+
+        int[] massive = new int[4]; // create an array with four elements
 
         //System.out.println(array[0]);
         //System.out.println(array.length);
 
-        for(int i = 0; i < array.length; i = i +1){
-            System.out.println(array[i]);
-
+        for(int i = 0; i < array.length; i++){
+           // System.out.println(array[i]);
+            System.out.println("-------------------");
+            System.out.println("Massive " + array[i] + 1);
         }
+        System.out.println(Arrays.toString(array)); //To print array
     }
 
 
